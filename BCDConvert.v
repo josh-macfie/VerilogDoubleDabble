a9cb48d31de447fb5005c97e357dffa5378d97a8
josh-macfie <felixx68@gmail.com>
2020-08-09 13:53:19 -0500

    first commit

---
+++ BCDConvert.v
@@ -0,0 +1,131 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/07/2020 03:46:57 PM
+// Design Name: 
+// Module Name: BCDConvert
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BCDConvert(
+    input           clk,
+    input           en,
+    input   [11:0]  bin_d_in,
+    output  [15:0]  bcd_d_out,
+    output          rdy
+    );
+    
+    //State variables
+    parameter IDLE      = 3'b000;
+    parameter SETUP     = 3'b001;
+    parameter ADD       = 3'b010;
+    parameter SHIFT     = 3'b011;
+    parameter DONE      = 3'b100;
+    
+    //reg [11:0]  bin_data    = 0;
+    reg [27:0]  bcd_data    = 0;
+    reg [2:0]   state       = 0;
+    reg         busy        = 0;
+    reg [3:0]   sh_counter  = 0;
+    reg         result_rdy  = 0;
+    
+    
+    always @(posedge clk)
+        begin
+        if(en)
+            begin
+                if(~busy)
+                    begin
+                    bcd_data    <= {16'b0, bin_d_in};
+                    state       <= SETUP;
+                    end
+            end
+        
+        case(state)
+        
+            IDLE:
+                begin
+                    result_rdy  <= 0;
+                    busy        <= 0;
+                end
+                
+            SETUP:
+                begin
+                busy        <= 1;
+                state       <= ADD;
+                end
+                    
+            ADD:
+                begin
+                
+                if(bcd_data[15:12] > 4)
+                    begin
+                        bcd_data[27:12] <= bcd_data[27:12] + 3;
+                    end
+                
+                if(bcd_data[19:16] > 4)
+                    begin
+                        bcd_data[27:16] <= bcd_data[27:16] + 3;
+                    end
+
+                if(bcd_data[23:20] > 4)
+                    begin
+                        bcd_data[27:20] <= bcd_data[27:20] + 3;
+                    end
+
+                if(bcd_data[27:24] > 4)
+                    begin
+                        bcd_data[27:24] <= bcd_data[27:24] + 3;
+                    end
+
+                state   <= SHIFT;
+
+                end
+                
+            SHIFT:
+                begin
+                sh_counter  <= sh_counter + 1;
+                bcd_data    <= bcd_data << 1;
+                
+                if(sh_counter == 11)
+                    begin
+                    sh_counter  <= 0;
+                    state       <= DONE;
+                    end
+                else
+                    begin
+                    state   <= ADD;
+                    end
+
+                end
+ 
+            
+            DONE:
+                begin
+                result_rdy  <= 1;
+                state       <= IDLE;
+                end
+            default:
+                begin
+                state <= IDLE;
+                end
+            
+            endcase
+            
+        end
+    assign bcd_d_out    = bcd_data[27:12];
+    assign rdy          = result_rdy;
+endmodule
---
+++ BDC_tb.v
@@ -0,0 +1,148 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/08/2020 11:40:57 AM
+// Design Name: 
+// Module Name: BDC_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BDC_tb(
+
+    );
+
+//Drivable inputs are registers
+reg         clk         = 0;
+reg         en          = 0;
+reg [11:0]  bin_d_in    = 0;
+
+//Readable outputs are wires
+wire [15:0] bcd_d_out;
+wire rdy;
+
+//Instantiate in the same order as your module decleration.
+//I personally copy straight from the module that will be instantiated and just edit it.
+BCDConvert BCD_uut(
+    .clk(clk),
+    .en(en),
+    .bin_d_in(bin_d_in),
+    .bcd_d_out(bcd_d_out),
+    .rdy(rdy)
+    );
+    
+//For this test we will need:
+//  a running clock             (clk)
+//  a changing enable signal    (en)
+//  a changing binary input     (bin_d_in)
+initial
+    begin
+    forever
+        begin
+        #10 clk = !clk; //10*2 for full wave
+        end
+    end
+
+initial
+    begin
+    forever
+        begin
+        bin_d_in    = 0;
+        en          = 1;
+        #20 //en must catch rising edge of clk
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = 12'b111111111111;//   2^(12)-1 = 4095 = 0xfff
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = 0;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 1;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 10;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 10;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 100;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 100;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 1000;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 1000;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 1;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 2;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 2;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+
+        bin_d_in    = bin_d_in + 5;
+        en          = 1;
+        #20
+        en          = 0;
+        #620; //IDEL (+1); SETUP (+1); ADD (+12); SHIFT (+12); DONE (+1); extra for edge case (+4); enable back to zero (-1) ~~> ((1+1+12+12+1+1+4-1)*10*2) = 620 
+        end
+    end
+
+endmodule
